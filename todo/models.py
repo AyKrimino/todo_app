60321c72de44f39fa667414fa499b0165b0fee18
AyKrimino <ayoub.krimi14@gmail.com>
2023-12-28 11:47:35 +0100

    Adding two models Task and Category
    I added the Task model inside the models.py file which is the main model of this app and and I assinged each Task to some User and then I created a Category model to have each Task categorized by some tags

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    STATUS_CHOICES = {
+        'pending': 'pending',
+        'completed': 'completed',
+        'in progress': 'in progress',
+    }
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    priority = models.IntegerField(default=0)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='pending')
+    assigned_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
