d6844390a63da71fd8aff809b47e27ede1ec6aca
AyKrimino <ayoub.krimi14@gmail.com>
2023-12-28 20:48:11 +0100

    Solving the error and Adding the create functionality
    Finallly I solved the previous error and added the create functionality to the app

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from .models import Task
 from .forms import TaskForm
 
@@ -9,7 +10,15 @@ def display_tasks(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_task = Task(
+                title=form.cleaned_data['title'],
+                description=form.cleaned_data['description'],
+                status=form.cleaned_data['status'],
+                assigned_user=User.objects.get(username='admin'),
+            )
+            new_task.save()
+            for category in form.cleaned_data['categories']:
+                new_task.categories.add(category)
         return redirect('display-tasks')
     
     tasks = Task.objects.all()
